917fd3debe035de1e881bb7662011a6a55cd4945
msm3673763 <1149665144@qq.com>
2013-07-21 23:19:33 +0800

    8489489489

---
+++ src/com/xxx/servlet/Register.java
@@ -0,0 +1,22 @@
+package com.xxx.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class Register extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+}
