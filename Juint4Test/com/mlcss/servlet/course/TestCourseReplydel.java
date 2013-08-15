461c2be76186a28797f502a4ff63646f8109e5a5
sida <Administrator@LBDZ-11071415>
2013-08-15 21:25:37 +0800

    成功

---
+++ Juint4Test/com/mlcss/servlet/course/TestCourseReplydel.java
@@ -0,0 +1,44 @@
+package com.mlcss.servlet.course;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+import org.junit.Test;
+/**
+ * @author jc
+ */
+
+public class TestCourseReplydel {
+
+	@Test
+	public void testReceive() throws Exception{
+		
+		URL url = new URL(
+				"http://localhost:8080/MoblicLearningCommunity/service/CourseDel");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("POST");
+		conn.setDoOutput(true);
+		conn.setDoInput(true);
+		conn.setRequestProperty("Content-Type",
+				"application/x-www-form-urlencoded");
+		
+		int id = 3;
+		
+		DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+		dos.write(("id=" + id).getBytes());
+		System.out.println(conn.getResponseCode());
+		dos.flush();
+		dos.close();   
+		
+		
+		BufferedReader dis = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		String injsonString = dis.readLine();
+		System.out.println(injsonString); 
+		dis.close();
+		conn.disconnect();
+	}
+}
